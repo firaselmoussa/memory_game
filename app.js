d3e6212ca16c4bf915973a617c4451d2ff0b3d32
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-09-16 18:42:40 +0300

    implemented img repitition prevention

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// const cards = document.getElementsByClassName('card');
+
 const cards = Object.values(document.getElementsByClassName('card'));
 const images = ['images/java-logo.jpg',
                 'images/javascript-logo.png',
@@ -6,12 +6,32 @@ const images = ['images/java-logo.jpg',
 const restart_btn = document.getElementById('restart');
 const score_span= document.getElementById('score')
 let score = 0;
+// The purpose of the functions below is to merely eliminate repitition in randome numbers.
+function getRandInt(min, max){
+
+    return  Math.floor(Math.random() * (max - min+1)) + min;
+
+}
+// create Range
+function createRange(start, end){
+    let range = [];
+    for(let i = start;  i <= end; i++){
+        range.push(i);
+    }
+    return range;
+}
+
+let numbers = createRange(0, images.length-1);
 
 // placing images
-for(i =1; i<= cards.length; i+=2){
-    var random_index = Math.floor(Math.random() * 3);
-    cards[i].setAttribute('src', images[random_index]);
-    cards[i-1].setAttribute('src', images[random_index]);
+for(i = 1; i<= cards.length; i+=2){
+
+    let randomIndex = getRandInt(0, numbers.length-1);
+    let randomNumber = numbers[randomIndex];
+    numbers.splice(randomIndex, 1);
+
+    cards[i].setAttribute('src', images[randomNumber]);
+    cards[i-1].setAttribute('src', images[randomNumber]);
 }
 
 window.addEventListener('load', ()=>{
